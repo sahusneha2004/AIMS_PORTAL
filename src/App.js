299d56639ff8c6f852e0f9a97d28d4ab0b275220
Sneha Sahu <sahusneha031@gmail.com>
2025-01-28 02:57:24 +0530

    changes done in app.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,80 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Landing from './pages/faculty/Landing'
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google"; // Import the OAuth provider
+import Login from "./pages/login";
+import LoginOptions from "./pages/loginOptions";
+import AdminLogin from "./pages/admin/admin";
+import AcademicEvents from "./pages/admin/academicEvents";
+import CoursesOfferedForEnrollment from "./pages/admin/coursesOfferedForEnrollment";
+import Departments from "./pages/admin/departments";
+import CoursesOfferedForOffering from "./pages/admin/coursesOfferedForOffering";
+import AcademicProfiles from "./pages/admin/academicProfiles";
+import { AuthProvider} from "./AuthContext";
+import ProtectedRoute from "./ProtectedRoute";
 
-function App(){
+function App() {
+  return (
+    <GoogleOAuthProvider clientId="356670906449-op3cr5hkdf4j3ppil0l7bhsbjvanecfg.apps.googleusercontent.com">
+      <Router>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+           
+            <Route path="/login-options" element={<LoginOptions />} />
+            <Route
+              path="/admin"
+              element={
+                <ProtectedRoute allowedRoles={["admin", "faculty"]}>
+                  <AdminLogin />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="admin/academic-events"
+              element={
+                <ProtectedRoute allowedRoles={["admin", "faculty"]}>
+                  <AcademicEvents />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="admin/courses-offered-for-enrollment"
+              element={
+                <ProtectedRoute allowedRoles={["admin"]}>
+                  <CoursesOfferedForEnrollment />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="admin/courses-offered-for-offering"
+              element={
+                <ProtectedRoute allowedRoles={["admin"]}>
+                  <CoursesOfferedForOffering />
+                </ProtectedRoute>
+              }
+            />
 
-  return <Landing />
+            <Route
+              path="admin/departments"
+              element={
+                <ProtectedRoute allowedRoles={["admin"]}>
+                  <Departments />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="admin/academic-profiles"
+              element={
+                <ProtectedRoute allowedRoles={["admin"]}>
+                  <AcademicProfiles />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
